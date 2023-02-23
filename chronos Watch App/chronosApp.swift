7d0ee7633d3942bca97bcbac4c261eee0b3251f4
danielferrer <danybarco@gmail.com>
2023-02-23 07:16:44 +0100

    Initial Commit

---
+++ chronos Watch App/chronosApp.swift
@@ -0,0 +1,17 @@
+//
+//  chronosApp.swift
+//  chronos Watch App
+//
+//  Created by Daniel Ferrer on 23/2/23.
+//
+
+import SwiftUI
+
+@main
+struct chronos_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
